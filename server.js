368bebdb640e5d3a0e1fc7a0d11e10bfb750386c
thespicesaga <thespicesagaindore@gmail.com>
2026-08-22 06:50:14 +0530

    Update server.js
    Register service worker

--- server.js
+++ server.js
@@ -418,7 +418,14 @@ async function pay(){
     );
   }
 }
-
+<script>
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/sw.js")
+      .then(() => console.log("Service Worker registered"))
+      .catch(err => console.error("SW registration failed:", err));
+  });
+}
 </script></body></html>`;
 
 app.get("/",(req,res)=>{
